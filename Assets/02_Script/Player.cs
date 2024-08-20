c0df07f23c6220ec867abc01ddbc2a0b4f9cb3e7
flyingore1115 <bbtw2235@gmail.com>
2024-08-20 20:44:13 +0900

    -프로젝트 기본 세팅 -플레이어 움직임 구현 - 플레이어 Follow 카메라(W/시네머신)

---
+++ Assets/02_Script/Player.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float MoveSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+    }
+    private void Movement()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(Vector3.forward * MoveSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(Vector3.left * MoveSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(Vector3.right * MoveSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(Vector3.back * MoveSpeed * Time.deltaTime);
+        }
+
+
+    }
+}
